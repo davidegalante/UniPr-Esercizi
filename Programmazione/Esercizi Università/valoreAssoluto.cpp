14ef5e59d25b4518deee52e9837c2c8644bf2cc1
Davide Galante <Davidegalante88@gmail.com>
2021-10-04 21:08:25 +0200

    nuovo es

---
+++ Programmazione/Esercizi Università/valoreAssoluto.cpp
@@ -0,0 +1,20 @@
+/******************************************************************************
+
+Davide Galante
+Dato un numero, trova il suo valore assoluto
+NO LIBRARIA MATH
+
+***************************************************************************/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x, result;
+    cout << "Inserire un numero: ";
+    cin >> x;
+    result = x>0 ? x : -x;
+    cout<<endl<<"Il valore assoluto di " <<x<<" e': "<<result<<endl;
+    return 0;
+}
