7d6ab2c6c2eed148cc68da77c6aa96a398682eaf
Davide Galante <Davidegalante88@gmail.com>
2021-10-11 19:38:13 +0200

    task

--- Programmazione/Esercizi Università/cifrario.cpp
+++ Programmazione/Esercizi Università/cifrario.cpp
@@ -20,13 +20,13 @@ int main()
     cout << "Inserire tre caratteri:" << endl;
     cin >> a >> b >> c;
 
-    a = (a >= 97 && a <= 122) ? (a+3) : 63; //Cifro primo numero, sostituisco carattere non valido con '?' (63, ascii table)
-    b = (b >= 97 && b <= 122) ? (b+3) : 63; //Cifro secondo numero, sostituisco carattere con '?' (63, ascii table)
-    c = (c >= 97 && c <= 122) ? (c+3) : 63; //Cifro terzo numero, sostituisco carattere con '?' (63, ascii table)
+    a = (a >= 97 && a <= 122) ? (a+3) : 63; //Cifro primo carattere, sostituisco carattere non valido con '?' (63, ascii table)
+    b = (b >= 97 && b <= 122) ? (b+3) : 63; //Cifro secondo carattere, sostituisco carattere non valido con '?' (63, ascii table)
+    c = (c >= 97 && c <= 122) ? (c+3) : 63; //Cifro terzo carattere, sostituisco carattere non valido con '?' (63, ascii table)
 
-    a = a < 122 ? a : a-26; //Se cifratura supera la lettera "z", torno all'inizio
-    b = b < 122 ? b : b-26; //Se cifratura supera la lettera "z", torno all'inizio
-    c = c < 122 ? c : c-26; //Se cifratura supera la lettera "z", torno all'inizio
+    a = a < 122 ? a : a-26; //Se cifratura supera la lettera "z", torno all'inizio 
+    b = b < 122 ? b : b-26; //Se cifratura supera la lettera "z", torno all'inizio 
+    c = c < 122 ? c : c-26; //Se cifratura supera la lettera "z", torno all'inizio 
 
     cout << "Cifrario di Cesare: "<< a << " " << b << " " << c << endl; //Stampa i caratteri cifrati
     return 0;
