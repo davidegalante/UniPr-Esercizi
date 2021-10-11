611561aabc316f6d050c12b9dc9b7009f9aac5ef
Davide Galante <Davidegalante88@gmail.com>
2021-10-11 19:06:20 +0200

    task

--- Programmazione/Esercizi Università/cifrario.cpp
+++ Programmazione/Esercizi Università/cifrario.cpp
@@ -22,7 +22,7 @@ int main()
 
     //Cifro primo carattere
     if (a < 97 || a > 122){ //Se il carattere non è una lettera minuscola (nella tabella ascii vanno da 97 a 122), allora sostituisci con un punto interrogativo
-        a = 63;
+        a = 63;             //Punto di domanda
     }else{                  //Se il carattere è valido, avanza di 3 posizioni
         a = a + 3;
         if(a > 122)         //Controllo se il risultato va oltre la lettera "z"
@@ -33,7 +33,7 @@ int main()
 
     //Cifro secondo carattere
     if (b < 97 || b > 122){ //Se il carattere non è una lettera minuscola (nella tabella ascii vanno da 97 a 122), allora sostituisci con un punto interrogativo
-        b = 63;  
+        b = 63;             //Punto di domanda
     }else{                  //Se il carattere è valido, avanza di 3 posizioni
         b = b + 3;
         if(b > 122)         //Controllo se il risultato va oltre la lettera "z"
@@ -44,7 +44,7 @@ int main()
 
     //Cifro terzo carattere
     if (c < 97 || c > 122){ //Se il carattere non è una lettera minuscola (nella tabella ascii vanno da 97 a 122), allora sostituisci con un punto interrogativo
-        c = 63;
+        c = 63;             //Punto di domanda
     }else{                  //Se il carattere è valido, avanza di 3 posizioni
         c = c + 3;
         if(c > 122)         //Controllo se il risultato va oltre la lettera "z"
