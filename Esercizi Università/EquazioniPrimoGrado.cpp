1c9c6936a3f253e65bd6fe9e5a2e89b0a147b570
Davide Galante <Davidegalante88@gmail.com>
2021-10-15 17:14:43 +0200

    esercizio equazioni

--- Esercizi Università/EquazioniPrimoGrado.cpp
+++ Esercizi Università/EquazioniPrimoGrado.cpp
@@ -21,16 +21,16 @@ int main()
     cin >> b;
 
     tipoEquazione = (a==0&&b!=0) ? "impossibile" : (a==0&&b==0) ? "indeterminata" : "determinata"; //Trovo il tipo dell'equazione
-    if (tipoEquazione == "determinata"){ //Trovo la soluzione, se c'è
-        x = -b/a;
+    if (tipoEquazione == "determinata"){    //Trovo la soluzione, se c'è
+        x = (-b/a) == -0 ? 0 : -(b/a);      //Per togliere il '-0', che penso non sia giustissimo
     }
 
-    cout << endl << "L'equazione e' " << tipoEquazione;
+    cout << endl << "L'equazione " << a << "x+" << b << "=0 e' " << tipoEquazione;
     if (tipoEquazione == "determinata"){    //Se è determinata, c'è una soluzione e la stampo.
         cout << endl << "La soluzione e': " << x;
     }else if (tipoEquazione == "indeterminata"){
         cout << endl << "Ha infinite soluzioni";
     }
-    
+
     return 0;
 }
